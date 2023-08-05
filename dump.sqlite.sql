b9cc6cb421a25929c8a591cb6bd5a36ce51762b7
Gia-Huy N Gonzalez <39535243+Yowzel@users.noreply.github.com>
2023-08-04 18:00:59 -0700

    Add files via upload

---
+++ dump.sqlite.sql
@@ -0,0 +1,10 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE "sleep_lifestyle" ('c1' TEXT,'c2' TEXT,'c3' TEXT,'c4' TEXT,'c5' TEXT,'c6' TEXT,'c7' TEXT,'c8' TEXT,'c9' TEXT,'c10' TEXT,'c11' TEXT,'c12' TEXT,'c13' TEXT);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
